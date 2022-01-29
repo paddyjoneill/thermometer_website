bb0f0e78d09d0fc16237c831a5aeb68b004ded4f
Paddy <patrick@firefinch.io>
2022-01-29 06:29:07 +0000

    added og tags

--- src/components/head/NextHead.tsx
+++ src/components/head/NextHead.tsx
@@ -5,12 +5,16 @@ import { getStaticProps } from "../../../pages";
 interface Props {
     pageTitle?: string
     description?: string
+    url?: string
+    openGraphPictureUrl?: string
 }
 
 const NextHead = (props: Props) => {
 
     const pageTitle = props.pageTitle ?? "AccSensum Thermometers"
     const description = props.description ?? "Downhole Thermometers"
+    const url = props.url ?? "https://thermometer-site.netlify.app"
+    const openGraphPictureUrl = props.openGraphPictureUrl ?? "http://patsstats.co.uk/images/facebook/patsstats-facebook-og.jpg"
 
     return <Head>
         <title>{pageTitle}</title>
@@ -19,21 +23,20 @@ const NextHead = (props: Props) => {
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
 
 
-    {/* facebook open graph stuff */}
-    {/* ToDo: make dynamic for different pages
-    <meta property="og:url" content="http://patsstats.co.uk" />
-    <meta property="og:type" content="website" />
-    {/* <meta property="fb:app_id" content="your fb app id" /> */}
-    {/* <meta
-      property="og:title"
-      content="Pat's Stats"
-    />
-    <meta name="twitter:card" content={description} />
-    <meta
-      property="og:description"
-      content={description}
-    />
-    <meta property="og:image" content={"http://patsstats.co.uk/images/facebook/patsstats-facebook-og.jpg"} /> */}
+        {/* facebook open graph stuff */}
+        <meta property="og:url" content={url} />
+        <meta property="og:type" content="website" />
+        {/* <meta property="fb:app_id" content="your fb app id" /> */}
+        <meta
+            property="og:title"
+            content="Pat's Stats"
+        />
+        <meta name="twitter:card" content={description} />
+        <meta
+            property="og:description"
+            content={description}
+        />
+        <meta property="og:image" content={openGraphPictureUrl} />
 
 
         <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
