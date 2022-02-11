5f6725749add103e1489e10f4e866578317cfd3f
Paddy <patrick@firefinch.io>
2022-02-11 15:46:18 +0000

    form validation

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -1,4 +1,5 @@
 import React, { ChangeEvent, useState } from "react";
+import * as EmailValidator from 'email-validator';
 
 const Contact = () => {
 
@@ -7,23 +8,44 @@ const Contact = () => {
   const [subject, setSubject] = useState<string>("")
   const [message, setMessage] = useState<string>("")
 
+  const [responseMessage, setResponseMessage] = useState<string>("")
+
   const updateName = (event: ChangeEvent<HTMLInputElement>) => setName(event.target.value)
   const updateEmail = (event: ChangeEvent<HTMLInputElement>) => setEmail(event.target.value)
   const updateSubject = (event: ChangeEvent<HTMLInputElement>) => setSubject(event.target.value)
   const updateMessage = (event: ChangeEvent<HTMLTextAreaElement>) => setMessage(event.target.value)
-
-  // const onSubmit = async (data: any, e: { target: { reset: () => void; }; }) => {
     
+  const updateResponseMessage = async (message: string) => {
+    setResponseMessage(message)
+    await setTimeout(() => setResponseMessage(""), 5000)
+  }
+
   const onSubmit = async (e: React.FormEvent) => {
     e.preventDefault();
 
-    // validation...
+    // validation
 
+    // name
+    if(name.length === 0){
+      updateResponseMessage("Please Enter a Name")
+      return;
+    }
+    // subject
+
+    // email
+    if(!EmailValidator.validate(email)){
+      updateResponseMessage("Invalid Email Address")
+      return;
+    } 
+
+    // message
+    if(message.length === 0){
+      updateResponseMessage("Please Enter a Message")
+      return;
+    }
 
     const apiUrl = "/api/contactForm"
-
     const payload = {name, email, subject, message}
-
     const postSettings = {
       method: 'POST', // *GET, POST, PUT, DELETE, etc.
       // mode: 'cors', // no-cors, *cors, same-origin
@@ -38,12 +60,19 @@ const Contact = () => {
     }
 
     const response = await fetch(apiUrl, postSettings)
-
-    // catch error or success...
-    
+    // catch error or success...   
     console.log({response});
 
-    // clear the input boxes if successful
+    if(response.status === 200){
+      updateResponseMessage("Message Sent Successfully")
+      // clear the input boxes if successful
+      setName("")
+      setEmail("")
+      setSubject("")
+      setMessage("")
+    } else {
+      updateResponseMessage("There was a problem sending your message. Please try again or email directly")
+    }
   };
 
 
@@ -109,9 +138,9 @@ const Contact = () => {
                 value={message}
                 onChange={updateMessage}
               ></textarea>
-              {/* {errors.message && (
-                <span className="invalid-feedback">Message is required.</span>
-              )} */}
+              {/* {responseMessage !== "" && ( */}
+                <span className="invalid-feedback">{responseMessage}</span>
+              {/* // )} */}
             </div>
           </div>
           {/* End .col */}
